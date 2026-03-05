4759fadb99d317a902c0cfd9d9055f010c7d9459
Duane Northcutt <duane.northcutt@gmail.com>
2026-03-04 18:22:46 -0800

    Created.

---
+++ src/adsbmon/Track.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+#
+# Object that encapsulates a vehicle track
+
+import json
+import logging
+import sys
+import time
+
+
+class Track:
+    def __init__(self, message, rxTime):
+        self.track = {}
+        self.update(message, rxTime)
+
+    def update(self, message, rxTime):
+        self.track['updateTime'] = time.time()
+        if 'seen_pos' in message.keys():
+            self.track['seenPosTime'] = rxTime - message['seen_pos']
+        if 'seen' in message.keys():
+            self.track['seenTime'] = rxTime - message['seen']
+        self.track.update(message)
+
+    def print(self):
+        json.dump(self.track, sys.stdout, indent=4, sort_keys=True)
+        print("")
