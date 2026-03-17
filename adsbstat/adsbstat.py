9c13cf8387897f10960e7b552a51c4eb00a5e85e
Duane Northcutt <duane.northcutt@gmail.com>
2026-03-16 17:37:47 -0700

    Added sigusr1 to print tracks.

--- adsbstat/adsbstat.py
+++ adsbstat/adsbstat.py
@@ -75,57 +75,8 @@ class ExitGracefully:
 
 
 def processTracks():
-    print("ProcessTracks")  #### FIXME
-'''
-            if {'lat', 'lon'} <= msg.keys():
-                distance = self.rxSite.distance2dNM(msg['lat'], msg['lon'])
-                logging.debug("distance: %f", distance)
-                if distance <= self.rxSite.max2dDistance:
-                    requiredKeys = {'alt_geom', 'category', 'gs', 'hex', 'seen_pos'}
-                    missingKeys = requiredKeys  - msg.keys()
-                    if missingKeys:
-                        logging.error("Message is missing fields: %s", missingKeys)
-                        continue
-
-                    additionalKeys = {'baro_rate', 'emergency', 'flight', 'geom_rate', 'rssi', 'seen'}
-                    hexId = msg['hex']
-                    mappings = self.aircraftDB.getMappings(hexId)
-                    if not mappings[0]:
-                        logging.error("HexId '%s' not found in AircraftDB", hexId)
-                        continue
-                    record = {
-                        'acType': mappings[2],
-                        'acCode': mappings[3],
-                        'dist2d': distance,
-                        'dist3d': self.rxSite.distance3dNM(msg['lat'], msg['lon'], msg['alt_geom']),
-                        'ts': data['now'],
-                        'tn': mappings[1]
-                    }
-                    requiredFields = {k: msg.get(k) for k in requiredKeys}
-                    record.update(requiredFields)
-                    if ADDITIONAL_FIELDS:
-                        addedFields = {k: msg.get(k) for k in additionalKeys}
-                        record.update(addedFields)
-
-                    if hexId in self.records:
-                        self.records[hexId].append(record)
-                    else:
-                        self.records[hexId] = [record]
-                    #### TMP TMP TMP
-                    for h, l in self.records.items():
-                        lastSeen = l[-1]['ts'] + l[-1]['seen_pos']
-                        print(f"> {h}: {len(l)}, {datetime.fromtimestamp(lastSeen)}")
-                    print("")
-
-                    # GC the track records
-                    for hexId, recordList in self.records.items():
-                        lastSeen = l[-1]['ts'] + l[-1]['seen_pos']
-                        now = time.time()
-                        print(f"{l[-1]['ts']}, {l[-1]['seen_pos']}, {now}")
-                        if now > lastSeen + STALE_TRACK_TIME:
-                            print(f"GC: {hexId}")
-                            del(self.records[hexId])
-'''
+    pass
+#    print("ProcessTracks")  #### FIXME
 
 
 def getOpts():
@@ -220,6 +171,11 @@ def run(options):
         rxSite.print()
 
     tracks = Tracks(aircraftDB, rxSite, STALE_TRACK_TIME)
+
+    def usr1Handler(sig, frame):
+        tracks.printAll()
+
+    signal.signal(signal.SIGUSR1, usr1Handler)   # 'kill -USR1' to print current tracks
 
     dumpDir = Path(options['adsbPath'])
 
