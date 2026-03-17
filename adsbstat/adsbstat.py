72334cc873d3649eafbeaeccd73dce1ae912b91a
Duane Northcutt <duane.northcutt@gmail.com>
2026-03-16 19:18:26 -0700

    Created closure for processMessage(). Inserted some commented code.

--- adsbstat/adsbstat.py
+++ adsbstat/adsbstat.py
@@ -74,11 +74,6 @@ class ExitGracefully:
                 logging.info("unknown signal: %s", sig)
 
 
-def processTracks():
-    pass
-#    print("ProcessTracks")  #### FIXME
-
-
 def getOpts():
     ap = argparse.ArgumentParser()
     ap.add_argument(
@@ -179,9 +174,21 @@ def run(options):
 
     dumpDir = Path(options['adsbPath'])
 
+    def createProcessMessage(aircraftDatabase, receiverSite):
+        ''' Returns a closure that captures instances of AircraftDB and ReceiverSite
+             for use in process message
+        '''
+        def processMessage(hexId, tracks):
+            acDB = aircraftDatabase
+            rx = receiverSite
+            print(f"process message: {hexId} #{len(tracks)}")  #### FIXME
+        return processMessage
+
+    processMsg = createProcessMessage(aircraftDB, rxSite)
+
     observer = PollingObserver()
     aircraftJsonPath = dumpDir / AIRCRAFT_JSON_FILE
-    handler = JsonFileHandler(aircraftJsonPath, tracks, processTracks)
+    handler = JsonFileHandler(aircraftJsonPath, tracks, processMsg)
     observer.schedule(handler, path=str(aircraftJsonPath), recursive=False)
     observer.start()
     logging.debug("Watching %s...", str(aircraftJsonPath))
@@ -206,3 +213,55 @@ def run(options):
 if __name__ == "__main__":
     opts = getOpts()
     run(opts)
+
+
+'''
+            if {'lat', 'lon'} <= msg.keys():
+                distance = self.rxSite.distance2dNM(msg['lat'], msg['lon'])
+                logging.debug("distance: %f", distance)
+                if distance <= self.rxSite.max2dDistance:
+                    requiredKeys = {'alt_geom', 'category', 'gs', 'hex', 'seen_pos'}
+                    missingKeys = requiredKeys  - msg.keys()
+                    if missingKeys:
+                        logging.error("Message is missing fields: %s", missingKeys)
+                        continue
+
+                    additionalKeys = {'baro_rate', 'emergency', 'flight', 'geom_rate', 'rssi', 'seen'}
+                    hexId = msg['hex']
+                    mappings = self.aircraftDB.getMappings(hexId)
+                    if not mappings[0]:
+                        logging.error("HexId '%s' not found in AircraftDB", hexId)
+                        continue
+                    record = {
+                        'acType': mappings[2],
+                        'acCode': mappings[3],
+                        'dist2d': distance,
+                        'dist3d': self.rxSite.distance3dNM(msg['lat'], msg['lon'], msg['alt_geom']),
+                        'ts': data['now'],
+                        'tn': mappings[1]
+                    }
+                    requiredFields = {k: msg.get(k) for k in requiredKeys}
+                    record.update(requiredFields)
+                    if ADDITIONAL_FIELDS:
+                        addedFields = {k: msg.get(k) for k in additionalKeys}
+                        record.update(addedFields)
+
+                    if hexId in self.records:
+                        self.records[hexId].append(record)
+                    else:
+                        self.records[hexId] = [record]
+                    #### TMP TMP TMP
+                    for h, l in self.records.items():
+                        lastSeen = l[-1]['ts'] + l[-1]['seen_pos']
+                        print(f"> {h}: {len(l)}, {datetime.fromtimestamp(lastSeen)}")
+                    print("")
+
+                    # GC the track records
+                    for hexId, recordList in self.records.items():
+                        lastSeen = l[-1]['ts'] + l[-1]['seen_pos']
+                        now = time.time()
+                        print(f"{l[-1]['ts']}, {l[-1]['seen_pos']}, {now}")
+                        if now > lastSeen + STALE_TRACK_TIME:
+                            print(f"GC: {hexId}")
+                            del(self.records[hexId])
+'''
