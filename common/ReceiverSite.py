4a4dd99eed65c46f0dac672d20c654dac1a9e4e0
Duane Northcutt <duane.northcutt@gmail.com>
2026-03-18 18:03:41 -0700

    Added methods for calculating distance constraints.

--- common/ReceiverSite.py
+++ common/ReceiverSite.py
@@ -1,42 +1,65 @@
 #!/usr/bin/python3
 #
-# Object that encapsulates information about the receiver site
+# Object that encapsulates information about the receiver site and provides
+#  functions for dealing with distance conversions and constraints.
 
-import json
 import math
-from pprint import pprint
-import sys
-import time
+from pprint import pformat
+
+from geographiclib.geodesic import Geodesic
+
+
+FEET_PER_NM = 6076.115
+METER_PER_NM = 1852.0
+KM_PER_NM = METER_PER_NM / 1000.0
+
+R_km = 6371.0088  # Earth radius in kilometers (mean)
+
+
+WGS84 = Geodesic.WGS84
 
 
 class ReceiverSite:
-    def __init__(self, name, max2dDistance, latitude, longitude, altitude):
-        self.name = name
-        self.max2dDistance = max2dDistance
-        self.lat = latitude
-        self.lon = longitude
-        self.alt = altitude
+    def __init__(self, name, rxPosition, groundConstriants, slantConstriants,
+                 verticalConstriants):
+        self.nameRx = name
+        self.rxPos = rxPosition
+        self.ground = groundConstriants
+        self.slant = slantConstriants
+        self.vertical = verticalConstriants
 
-    def print(self):
+    def __repr__(self):
         state = {
-            'name': self.name,
-            'maxDistance': self.max2dDistance,
-            'location': {
-                'latitude': self.lat,
-                'longitude': self.lon,
-                'altitude': self.alt
-            }
+            'name': self.nameRx,
+            'rxLocation': self.rxPos,
+            'ground': self.ground,
+            'slant': self.slant,
+            'vertical': self.vertical
         }
-        pprint(state, width=1, sort_dicts=False)
+        return pformat(state, indent=4, width=1)
 
-    def _distanceNM(self, lat1, lon1, lat2, lon2):
+    def getConstraints(self):
+        constraints = {
+            'ground': self.ground,
+            'slant': self.slant,
+            'vertical': self.vertical
+        }
+        return constraints
+
+    def setGroundConstriants(self, constraints):
+        self.ground = constraints
+
+    def setSlantConstriants(self, constraints):
+        self.slant = constraints
+
+    def setVerticalConstriants(self, constraints):
+        self.vertical = constraints
+
+    @staticmethod
+    def _haversineDistanceNM(lat1, lon1, lat2, lon2):
         """ Distance between two WGS84 coordinates in nautical miles.
             lat/lon is given in decimal degrees
         """
-        # Earth radius in kilometers (mean)
-        R_km = 6371.0088
-
-        # Convert degrees to radians
         phi1 = math.radians(lat1)
         phi2 = math.radians(lat2)
         dphi = math.radians(lat2 - lat1)
@@ -49,13 +72,61 @@ class ReceiverSite:
 
         d_km = R_km * c  # Distance in km
 
-        # convert km to nautical miles (1 NM = 1.852 km)
-        d_nm = d_km / 1.852
+        d_nm = d_km / KM_TO_NM
         return d_nm
 
-    def distance2dNM(self, lat, lon):
-        return self._distanceNM(self.lat, self.lon, lat, lon)
+    @staticmethod
+    def _geodesicDistanceNM(lat1, lon1, lat2, lon2):
+        ''' High-precision ellipsoidal distance in nautical miles
+        '''
+        result = WGS84.Inverse(lat1, lon1, lat2, lon2)
+        meters = result["s12"]
+        return meters / METER_PER_NM
 
-    def distance3dNM(self, lat, lon, alt):
-        altNM = alt / 6076.115
-        return math.sqrt((self.distance2dNM(lat, lon) ** 2) + (altNM ** 2))
+    @staticmethod
+    def _groundDistanceNM(lat, lon):
+        return _geodesicDistanceNM(self.rxPos.latitude, self.rxPos.longitude, lat, lon)
+
+    @staticmethod
+    def _slantDistanceNM(lat, lon, alt):
+        altNM = alt / FEET_PER_NM
+        return math.sqrt((ReceiverSite._groundDistanceNM(lat, lon) ** 2) + (altNM ** 2))
+
+    @staticmethod
+    def _withinConstraints(value, constraints):
+        if constraints.min is not None and value < constraints.min:
+            return False
+        if constraints.max is not None and value > constraints.max:
+            return False
+        return True
+
+    def surfaceDistanceToTarget(self, target):
+        return ReceiverSite._geodesicDistanceNM(self.rxPos.latitude, self.rxPos.longitude,
+                                                target.lat, target.lon)
+
+    def withinVolume(self, target):
+        ''' ?
+            ground (min_nm, max_nm)
+            slant (min_nm, max_nm)
+            altitude (min_ft, max_ft)
+        '''
+        lat1, lon1, alt1 = self.rxPos
+        lat2, lon2, alt2 = target
+
+        groundDistNM = ReceiverSite._geodesicDistanceNM(lat1, lon1, lat2, lon2)
+
+        verticalDistFt = abs(alt2 - alt1)
+        verticalDistNM = verticalDistFt / FEET_PER_NM
+
+        slantDistNM = math.sqrt(groundDistNM**2 + verticalDistNM**2)
+
+        if self.ground is not None:
+            if not ReceiverSite._withinConstraints(groundDistNM, self.ground):
+                return False
+        if self.slant is not None:
+            if not ReceiverSite._withinConstraints(slantDistNM, self.slant):
+                return False
+        if self.vertical is not None:
+            if not ReceiverSite._withinConstraints(verticalDistFt, self.vertical):
+                return False
+        return True
