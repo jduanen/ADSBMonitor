ce49fd978841b2a6394bae5128468e0bf3704250
J. Duane Northcutt <duane.northcutt@gmail.com>
2026-04-09 17:36:11 -0700

    Fixed topic and comments.

--- scripts/printVehicleCount.sh
+++ scripts/printVehicleCount.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-# Script to print count of ADS-B vehicles within the range of interest
+# Script to print count of ADS-B vehicles detected by the receiver
 
 MQTT_USER=$1
 MQTT_PASSWD=$2
-OPTIONS=$3  # e.g., '-v'
+OPTIONS=$3  # e.g., '-v' to print the topic
 
 if [ $# -lt 2 ]; then
     echo "Usage: $0 <mqttUser> <mqttPassword> {<options>}"
     exit 1
 fi
 
-mosquitto_sub -h 192.168.166.5 -p 1883 -u $MQTT_USER -P $MQTT_PASSWD ${OPTIONS} -t 'adsb/monitor/in_range'
+mosquitto_sub -h 192.168.166.5 -p 1883 -u $MQTT_USER -P $MQTT_PASSWD ${OPTIONS} -t 'adsb/monitor/count'
