8cc63f88c274b4e5e7cd00e17a5dd6137e7b8f9d
J. Duane Northcutt <duane.northcutt@gmail.com>
2026-04-09 17:25:27 -0700

    Created.

---
+++ scripts/getRetainedMsgs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Script to get all of the retained messages for sensors from the HomeAssistant broker
+
+MQTT_USER=$1
+MQTT_PASSWD=$2
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <mqttUser> <mqttPassword>"
+    exit 1
+fi
+
+mosquitto_sub -h 192.168.166.5 -p 1883 -u $MQTT_USER -P $MQTT_PASSWD --retained-only -v -t 'homeassistant/sensor/#'
