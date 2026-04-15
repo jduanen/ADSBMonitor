6cd4ae08c23e222c58864c2a7d499d1cbdf9fccc
J. Duane Northcutt <duane.northcutt@gmail.com>
2026-04-14 17:58:58 -0700

    Changed topic.

--- scripts/printTrackingVehicles.sh
+++ scripts/printTrackingVehicles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Script to print count of ADS-B vehicles within the range of interest
+# Script to print count of ADS-B vehicles being tracked (within the given volume)
 
 MQTT_USER=$1
 MQTT_PASSWD=$2
@@ -11,4 +11,4 @@ if [ $# -lt 2 ]; then
     exit 1
 fi
 
-mosquitto_sub -h 192.168.166.5 -p 1883 -u $MQTT_USER -P $MQTT_PASSWD ${OPTIONS} -t 'adsb/monitor/in_range'
+mosquitto_sub -h 192.168.166.5 -p 1883 -u $MQTT_USER -P $MQTT_PASSWD ${OPTIONS} -t 'adsb/monitor/tracking'
