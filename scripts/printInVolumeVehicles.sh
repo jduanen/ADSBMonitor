9d55726411940064c225eb2c300c0f3d41921e52
Duane Northcutt <duane.northcutt@gmail.com>
2026-04-16 12:30:15 -0700

    Renamed.

---
+++ scripts/printInVolumeVehicles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Script to print count of ADS-B vehicles being tracked (within the given volume)
+
+mqttUser="${1:-${MQTT_USER}}"
+mqttPasswd="${2:-${MQTT_PASSWD}}"
+options=$3  # e.g., '-v' to print the topic
+
+if [[ -z "$mqttUser" || -z "$mqttPasswd" ]]; then
+    echo "Usage: $0 <mqttUser> <mqttPassword> {<options>}"
+    exit 1
+fi
+
+mosquitto_sub -h 192.168.166.5 -p 1883 -u $mqttUser -P $mqttPasswd $OPTIONS -t 'adsb/monitor/in_volume'
