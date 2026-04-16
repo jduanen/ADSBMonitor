35764a321d6f1a54cc0f6525291e1db9c3754649
Duane Northcutt <duane.northcutt@gmail.com>
2026-04-16 12:34:28 -0700

    Made take slant distance arg.

--- scripts/startAdsbMqtt.sh
+++ scripts/startAdsbMqtt.sh
@@ -2,13 +2,13 @@
 #
 # Script to start the ADS-B MQTT publisher
 #
-# Usage: ${0} [<distance> [<logLevel>] [<options>]]]
+# Usage: ${0} [<slantDistance> [<logLevel>] [<options>]]]
 
-DISTANCE="${1:-2.0}"
+SLANT_DISTANCE="${1:-2.0}"
 LOG_LEVEL="${2:-WARNING}"
 OPTIONS="${3}"  # e.g., '-vvv'
 
 SCRIPT_PATH="${HOME}/Code/ADSBMonitor/adsbmon"
 CONF_FILE_PATH="${HOME}/Code/ADSBMonitor/adsbmon"
 
-python3 ${SCRIPT_PATH}/adsbmon.py -c ${CONF_FILE_PATH}/config.yaml ${OPTIONS} -L ${LOG_LEVEL} -D ${DISTANCE} /run/readsb/
+python3 ${SCRIPT_PATH}/adsbmon.py -c ${CONF_FILE_PATH}/config.yaml ${OPTIONS} -L ${LOG_LEVEL} -s ${SLANT_DISTANCE} /run/readsb/
