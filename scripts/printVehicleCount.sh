5cc11012a255d3f416ae59d9df6f48401372186b
Duane Northcutt <duane.northcutt@gmail.com>
2026-04-16 12:32:33 -0700

    Take env vars or CLI args as input.

--- scripts/printVehicleCount.sh
+++ scripts/printVehicleCount.sh
@@ -2,13 +2,13 @@
 #
 # Script to print count of ADS-B vehicles detected by the receiver
 
-MQTT_USER=$1
-MQTT_PASSWD=$2
+mqttUser="${1:-${MQTT_USER}}"
+mqttPasswd="${2:-${MQTT_PASSWD}}"
 OPTIONS=$3  # e.g., '-v' to print the topic
 
-if [ $# -lt 2 ]; then
+if [[ -z "$mqttUser" || -z "$mqttPasswd" ]]; then
     echo "Usage: $0 <mqttUser> <mqttPassword> {<options>}"
     exit 1
 fi
 
-mosquitto_sub -h 192.168.166.5 -p 1883 -u $MQTT_USER -P $MQTT_PASSWD ${OPTIONS} -t 'adsb/monitor/count'
+mosquitto_sub -h 192.168.166.5 -p 1883 -u $mqttUser -P $mqttPasswd $OPTIONS -t 'adsb/monitor/count'
