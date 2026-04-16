2be319f94e32cd746172e7d3717fcfdda40d6aac
Duane Northcutt <duane.northcutt@gmail.com>
2026-04-16 13:03:36 -0700

    Clean up.

--- scripts/printRetainedMsgs.sh
+++ scripts/printRetainedMsgs.sh
@@ -2,12 +2,14 @@
 #
 # Script to print the retained messages for sensors from the HomeAssistant broker
 
-MQTT_USER=$1
-MQTT_PASSWD=$2
+mqttUser="${1:-${MQTT_USER}}"
+mqttPasswd="${2:-${MQTT_PASSWD}}"
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <mqttUser> <mqttPassword>"
+OPTIONS= # "-v"
+
+if [[ -z "$mqttUser" || -z "$mqttPasswd" ]]; then
+    echo "Usage: $0 <mqttUser> <mqttPassword> {<options>}"
     exit 1
 fi
 
-mosquitto_sub -h 192.168.166.5 -p 1883 -u $MQTT_USER -P $MQTT_PASSWD --retained-only -v -t 'homeassistant/sensor/#'
+mosquitto_sub -h 192.168.166.5 -p 1883 -u $mqttUser -P $mqttPasswd --retained-only ${OPTIONS}-t 'homeassistant/sensor/#' | jq '.'
